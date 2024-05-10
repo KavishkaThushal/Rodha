37f0e5e770119c63aaf4db2c9310afb93e40ebe3
Kavishka Thushal <128395749+KavishkaThushal@users.noreply.github.com>
2024-05-10 09:39:05 +0530

    bug fix

--- client/src/Pages/SignUp/SignUp.jsx
+++ client/src/Pages/SignUp/SignUp.jsx
@@ -12,11 +12,14 @@ function SignIn() {
       
       <div className='flex  flex-1 flex-col p-3  gap-4 sm:gap-10  justify-center rounded-md  bg-white' >
         <div className='flex flex-col items-center '>
-          <h1 className='font-medium text-lg sm:text-2xl'>Welcome</h1>
+          <h1 className='font-medium text-lg sm:text-2xl'>Create an Account</h1>
           <p className='font-semibold  text-xs text-center text-slate-400'>Please enter your details.</p>
           </div>
           <form className='flex flex-col gap-2 justify-center items-center w-full'>
-            
+            <div className='flex flex-col gap-1'>
+              <label for="username" className='font-medium text-xs'>User Name</label>
+              <input type="text" id="username" name="username" placeholder="Enter your email" required className='flex px-2 py-1 rounded-md text-xs h-8 w-[30vh] sm:w-[35vh] border-2 border-blue-200 focus:outline-none hover:scale-105 transition-all' />
+            </div>
             <div  className='flex flex-col gap-1'>
               <label for="email" className='font-medium text-xs'>Email</label>
               <input type="email" id="email" name="email" placeholder="Enter your email" required className='flex px-2 py-1 rounded-md text-xs h-8 w-[30vh] sm:w-[35vh] border-2 border-blue-200 focus:outline-none hover:scale-105 transition-all'/>
@@ -26,14 +29,13 @@ function SignIn() {
               <input type="password" id="password" name="password" placeholder="Enter your password" required className='flex px-2 py-1 rounded-md text-xs h-8 w-[30vh] sm:w-[35vh] border-2 border-blue-200 focus:outline-none hover:scale-105 transition-all'/>
             </div>
             <div className='flex flex-col gap-3 mt-5'>
-            <button type="submit" className='flex px-2 py-2 justify-center items-center font-semibold text-white bg-blue-500 rounded-md text-sm w-[30vh] sm:w-[35vh] focus:outline-none hover:bg-blue-700'>Sign in</button>
+            <button type="submit" className='flex px-2 py-2 justify-center items-center font-semibold text-white bg-blue-500 rounded-md text-sm w-[30vh] sm:w-[35vh] focus:outline-none hover:bg-blue-700'>Sign up</button>
             <button type="submit" className='flex px-2 flex-row gap-2 py-2 justify-center items-center font-semibold text-black border-2 border-blue-500  rounded-md text-sm [30vh] sm:w-[35vh] focus:outline-none hover:bg-blue-200'>
               <img src={google} alt="google" className='w-5 h-5'/>
               Continue with Google</button>
             </div>
-            <span className='flex text-xs gap-1'>Don't have an account<button className='flex text-xs font-semibold text-blue-500'>Sign up</button></span>
+            <span className='flex text-xs gap-1'>All ready have an account?<button className='flex text-xs font-semibold text-blue-500'>Sign in</button></span>
           </form>
-          
       </div>
       <div className=' hidden sm:visible sm:flex flex-1 bg-slate-400 rounded-md'>
          <img src={Road2D} alt="road" className='object-cover w-full h-full rounded-md'/>
