26c0ee0e0f86812149e8275c7d410477e30cc499
Kavishka Thushal <128395749+KavishkaThushal@users.noreply.github.com>
2024-05-12 15:04:24 +0530

    creare redux-persistore

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,11 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import UserReducer from './Reducer/UserReducer'
+import {persistReducer, persistStore} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const rooterReducer = combineReducers({
+  user: UserReducer,
+})
+
+const persistConfig={
+  key:'Root',
+  storage,
+  version:1
+}
+
+const persistedReducers=persistReducer(persistConfig,rooterReducer)
 export const store = configureStore({
-  reducer: {
-    user: UserReducer,
-  },
+  reducer: persistedReducers,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
     
   })
 })
+
+export const persistor = persistStore(store)
